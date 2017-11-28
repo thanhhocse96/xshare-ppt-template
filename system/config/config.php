7fdbb7bfd376d2e8785744ba14b02b08a0581073
Unknown <thanh.hoquangcse96@gmail.com>
2017-11-28 19:34:53 +0700

    Update config.php

---
+++ system/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    //Database
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD',);
+
+    //Default Controller & Action 
+    define('CONTROLLER_DEFAULT', 'index');
+    define('ACTION_DEFAULT', 'index');
+    
+    
+?>
