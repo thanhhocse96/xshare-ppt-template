9b37daabb601d90bacfb4731fb5bcba4bc94d530
Unknown <thanh.hoquangcse96@gmail.com>
2017-12-06 23:23:02 +0700

    Add comment on SQL file

--- UI/src/xshare.sql
+++ UI/src/xshare.sql
@@ -6,6 +6,9 @@ CREATE TABLE `users` (
    `apiKey` varchar(32),
    PRIMARY KEY(id)
 );
+/*! User1 - pass: 123 */
 INSERT INTO users VALUES(1,'user1','202cb962ac59075b964b07152d234b70');
+/*! User2 - pass: 234 */
 INSERT INTO users VALUES(2,'user2','289dff07669d7a23de0ef88d2f7129e7');
+/*! User3 - pass: 345 */
 INSERT INTO users VALUES(3,'user3','d81f9c1be2e08964bf9f24b15f0e4900');
