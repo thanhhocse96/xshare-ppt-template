f31e48ea29a5382d0058084e22fab0c734424737
HoNgocSon <hcmct2@gmail.com>
2017-12-07 08:59:36 +0700

    edit

--- UI/src/xshare.sql
+++ UI/src/xshare.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `xshare`;
 USE `xshare`;
 CREATE TABLE `users` (
-   `id` int UNIQUE NOT NULL,
+   `id` int UNIQUE NOT NULL AUTO_INCREMENT,
    `username` varchar(40),
    `apiKey` varchar(32),
    `firstname` varchar(60),
@@ -10,10 +10,10 @@ CREATE TABLE `users` (
    PRIMARY KEY(id)
 );
 -- User1 - pass: 123
-INSERT INTO users VALUES(1,'user1','202cb962ac59075b964b07152d234b70', 'Hai', 'Le', 'user1@gmail.com');
+INSERT INTO users VALUES(0,'user1','202cb962ac59075b964b07152d234b70', 'Hai', 'Le', 'user1@gmail.com');
 -- User2 - pass: 123
-INSERT INTO users VALUES(2,'user2','202cb962ac59075b964b07152d234b70', 'Hien', 'Huynh', 'user2@gmail.com');
+INSERT INTO users VALUES(1,'user2','202cb962ac59075b964b07152d234b70', 'Hien', 'Huynh', 'user2@gmail.com');
 -- User3 - pass: 123
-INSERT INTO users VALUES(3,'user3','202cb962ac59075b964b07152d234b70', 'Son', 'Ho', 'user3@gmail.com');
+INSERT INTO users VALUES(2,'user3','202cb962ac59075b964b07152d234b70', 'Son', 'Ho', 'user3@gmail.com');
 -- User4 - pass: 123
-INSERT INTO users VALUES(4,'user4','202cb962ac59075b964b07152d234b70', 'Thanh', 'Ho', 'user4@gmail.com');
+INSERT INTO users VALUES(3,'user4','202cb962ac59075b964b07152d234b70', 'Thanh', 'Ho', 'user4@gmail.com');
