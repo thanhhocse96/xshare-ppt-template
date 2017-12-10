30d3381bddbb39a6635454f7dedbdeb08d2a17b4
Unknown <thanh.hoquangcse96@gmail.com>
2017-12-10 19:17:06 +0700

    Upload

--- src/xshare.sql
+++ src/xshare.sql
@@ -29,12 +29,12 @@ CREATE TABLE `products` (
 );
 
 
--- User1 - pass: 123456789 - normal
-INSERT INTO users VALUES(0,'user1','25f9e794323b453885f5181f1b624d0b', 'Hai', 'Le', 'user1@gmail.com', ' ');
--- User2 - pass: 123456789 - normal
-INSERT INTO users VALUES(1,'user2','25f9e794323b453885f5181f1b624d0b', 'Hien', 'Huynh', 'user2@gmail.com', ' ');
--- User3 - pass: 123456789 - admin
-INSERT INTO users VALUES(2,'user3','25f9e794323b453885f5181f1b624d0b', 'Son', 'Ho', 'user3@gmail.com', '*');
--- User4 - pass: 123456789 - normal
-INSERT INTO users VALUES(3,'user4','25f9e794323b453885f5181f1b624d0b', 'Thanh', 'Ho', 'user4@gmail.com', ' ');
+-- User001 - pass: 123456789 - normal
+INSERT INTO users VALUES(0,'user001','25f9e794323b453885f5181f1b624d0b', 'Hai', 'Le', 'user1@gmail.com', ' ');
+-- User002 - pass: 123456789 - normal
+INSERT INTO users VALUES(1,'user002','25f9e794323b453885f5181f1b624d0b', 'Hien', 'Huynh', 'user2@gmail.com', ' ');
+-- User003 - pass: 123456789 - admin
+INSERT INTO users VALUES(2,'user003','25f9e794323b453885f5181f1b624d0b', 'Son', 'Ho', 'user3@gmail.com', '*');
+-- User004 - pass: 123456789 - normal
+INSERT INTO users VALUES(3,'user004','25f9e794323b453885f5181f1b624d0b', 'Thanh', 'Ho', 'user4@gmail.com', ' ');
 
